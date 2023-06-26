6d3d01a824e5c4dfa6898bc6b5b6a5bbbe09d459
Franklinshtein <98875248+Franklinshtein@users.noreply.github.com>
2023-06-26 17:55:37 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import ContactForm from '../components/ContactForm/ContactForm';
 import Filter from '../components/Filter/Filter';
 import ContactList from '../components/ContactList/ContactList';
@@ -6,29 +6,36 @@ import styles from '../components/App.module.css';
 import { Notyf } from 'notyf';
 import 'notyf/notyf.min.css';
 
-function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
-  const [filter, setFilter] = useState('');
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ],
+      filter: '',
+    };
+    this.notyf = new Notyf();
+  }
 
-  const notyf = new Notyf();
-
-  useEffect(() => {
+  componentDidMount() {
     const storedContacts = localStorage.getItem('contacts');
     if (storedContacts) {
-      setContacts(JSON.parse(storedContacts));
+      this.setState({ contacts: JSON.parse(storedContacts) });
     }
-  }, []);
+  }
 
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
-  const addContact = (newContact) => {
+  addContact = (newContact) => {
+    const { contacts } = this.state;
     const isDuplicateName = contacts.some(
       (contact) => contact.name.toLowerCase() === newContact.name.toLowerCase()
     );
@@ -38,38 +45,59 @@ function App() {
     );
 
     if (isDuplicateName) {
-      notyf.error(`${newContact.name} is already in the phonebook.`);
+      this.notyf.error(`${newContact.name} is already in the phonebook.`);
       return;
     }
 
     if (isDuplicateNumber) {
-      notyf.error(`${newContact.number} is already in the phonebook.`);
+      this.notyf.error(`${newContact.number} is already in the phonebook.`);
       return;
     }
 
-    setContacts([...contacts, newContact]);
-    notyf.success('Contact added successfully!');
+    this.setState(
+      (prevState) => ({
+        contacts: [...prevState.contacts, newContact],
+      }),
+      () => {
+        this.notyf.success('Contact added successfully!');
+      }
+    );
   };
 
-  const deleteContact = (id) => {
-    const updatedContacts = contacts.filter((contact) => contact.id !== id);
-    setContacts(updatedContacts);
-    notyf.success('Contact deleted successfully!');
+  deleteContact = (id) => {
+    this.setState((prevState) => {
+      const updatedContacts = prevState.contacts.filter((contact) => contact.id !== id);
+      return { contacts: updatedContacts };
+    }, () => {
+      this.notyf.success('Contact deleted successfully!');
+    });
   };
 
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  handleFilterChange = (filter) => {
+    this.setState({ filter });
+  };
 
-  return (
-    <div className={styles.contact_list}>
-      <h1 className={styles.phonebook}>Phonebook</h1>
-      <ContactForm addContact={addContact} contacts={contacts} className={styles.form} />
+  render() {
+    const { contacts, filter } = this.state;
+
+    const filteredContacts = contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+    return (
+      <div className={styles.contact_list}>
+        <h1 className={styles.phonebook}>Phonebook</h1>
+        <ContactForm
+        addContact={this.addContact}
+        contacts={contacts}
+        className={styles.form}
+      />
       <h2 className={styles.segment}>Contacts</h2>
-      <Filter filter={filter} setFilter={setFilter} />
-      <ContactList contacts={filteredContacts} deleteContact={deleteContact} />
+      <Filter filter={filter} setFilter={this.handleFilterChange} />
+      <ContactList contacts={filteredContacts} deleteContact={this.deleteContact} />
     </div>
   );
+}
 }
 
 export default App;
